53eb35f047f9505ea25266ba3d92d3e579571de5
sujitsk2912 <skhangar321@gmail.com>
2024-03-15 16:59:09 +0530

    update Manage team

--- Controllers/ManageTeamController.cs
+++ Controllers/ManageTeamController.cs
@@ -20,43 +20,5 @@ namespace CricketWebApplicationMVC.Controllers
             var Team = dbHandler.GetRecords().Find(get => get.TeamID == TeamID);
             return View(Team);
         }
-      
-
-        /* [HttpGet]
-         public IActionResult ManageTeam(int TeamId)
-         {
-             ManageTeamDBHandler dBHandler = new ManageTeamDBHandler();
-             return View(dBHandler.GetRecords().Find(getDetails => getDetails.TeamID == TeamId));
-         }*/
-
-
-        /* public IActionResult ManageTeam(int TeamID)
-         {
-             // Check if TeamID is provided
-             if (TeamID.HasValue)
-             {
-                 ManageTeamDBHandler dbHandler = new ManageTeamDBHandler();
-                 AddTeamModel teamModel = dbHandler.GetRecords(TeamID.Value);
-
-                 // Check if teamModel is not null
-                 if (teamModel != null)
-                 {
-                     // Pass the retrieved data to the view
-                     return View(teamModel);
-                 }
-                 else
-                 {
-                     // Handle the case where no records are found for the provided TeamID
-                     TempData["AlertMessage"] = "No team found for the provided ID.";
-                     return View(new ManageTeamModel()); // Assuming ManageTeamModel is the model for your view
-                 }
-             }
-             else
-             {
-                 // Handle the case where TeamID is not provided
-                 return View(new ManageTeamModel()); // Assuming ManageTeamModel is the model for your view
-             }
-         }*/
-
     }
 }
